06b96baf51912e7f44cc301bd8fa31173a809b5f
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2023-08-15 20:44:36 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import CustomList from './lib/CustomList.svelte';
+	import type { ListItem } from './lib/types';
+
+	const items: ListItem[] = [
+		{
+			title: 'Svelte.dev',
+			link: 'https://svelte.dev',
+		},
+		{
+			title: 'TypeScript ESLint',
+			link: 'https://typescript-eslint.io',
+		},
+		{
+			title: 'TypeScript',
+			link: 'https://www.typescriptlang.org',
+		},
+	];
+
+	const openLink = (item: ListItem) => {
+		window.location.replace(item.link);
+	};
+</script>
+
+<main>
+	<CustomList
+		{items}
+		let:item
+	>
+		<div>
+			{item.title}
+			<button on:click={() => openLink(item)}>Open Link</button>
+		</div>
+	</CustomList>
+</main>
---
+++ src/lib/CustomList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { ListItem } from './types';
+
+	export let items: ListItem[];
+</script>
+
+<div>
+	<ul>
+		{#each items as item (item.title)}
+			<li>
+				<slot {item} />
+			</li>
+		{/each}
+	</ul>
+</div>
